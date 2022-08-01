3df2ccdcdce6db7fd166f5ff9ad5ed158ef05d19
Alexei <avenge.now.r@mail.ru>
2022-08-02 00:50:48 +0300

    Edit: Task Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+
+    let textNumber = Math.abs(number).toString();
+    let numberLength = textNumber.length;
+    let reverseNumber = ''; 
+
+    while (numberLength > 0) {
+        numberLength--;
+        reverseNumber = `${reverseNumber}${textNumber[numberLength]}`;
+    }
+
+    return +reverseNumber;
 }
